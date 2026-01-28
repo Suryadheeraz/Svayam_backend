6d35e92a3220fc724c395299581cd59b148b335a
FIXED-TERM Ralli Venkata Surya Dheeraz (BGSW/PJ-ETA-P) <lyv2kor@bosch.com>
2026-01-28 15:28:19 +0530

    cosmos_store

--- chatbot/cosmos_store.py
+++ chatbot/cosmos_store.py
@@ -727,7 +727,7 @@ def get_cosmos_client() -> CosmosClient:
         _cosmos_client = CosmosClient(
             COSMOS_ENDPOINT,
             COSMOS_KEY,
-            connection_timeout=10,
+            connection_timeout=15,
             request_timeout=30
         )
 
